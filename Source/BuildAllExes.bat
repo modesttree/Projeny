61a69397fc75b796a8f4127669bf98b21ff93ab6
Steve Vermeulen <sfvermeulen@gmail.com>
2016-01-10 16:17:04 -0400

    Changed to use winpython for the 32 bit python to use when creating exe builds

--- Source/BuildAllExes.bat
+++ Source/BuildAllExes.bat
@@ -1,6 +1,6 @@
-@echo off
+REM @echo off
 REM Make sure to use 32 bit python for this so it runs on all machines
-C:\Utils\Python\Python-34-32\python ./BuildPrjExeSetup.py py2exe
-C:\Utils\Python\Python-34-32\python ./BuildEditorApiExeSetup.py py2exe
-C:\Utils\Python\Python-34-32\python ./BuildReleaseManifesterUpdaterExeSetup.py py2exe
-C:\Utils\Python\Python-34-32\python ./BuildOpenInVisualStudio.py py2exe
+C:\Utils\Python\WinPython-3.4.3.7-32\python-3.4.3\python ./BuildPrjExeSetup.py py2exe
+C:\Utils\Python\WinPython-3.4.3.7-32\python-3.4.3\python ./BuildEditorApiExeSetup.py py2exe
+C:\Utils\Python\WinPython-3.4.3.7-32\python-3.4.3\python ./BuildReleaseManifesterUpdaterExeSetup.py py2exe
+C:\Utils\Python\WinPython-3.4.3.7-32\python-3.4.3\python ./BuildOpenInVisualStudio.py py2exe
