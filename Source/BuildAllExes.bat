f1118724fe5a5664e83f63a598a5c4e1952439ae
Steve Vermeulen <sfvermeulen@gmail.com>
2016-06-03 16:38:58 -0700

    Incremented version

--- Source/BuildAllExes.bat
+++ Source/BuildAllExes.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Make sure to use 32 bit python for this so it runs on all machines
-C:\Utils\Python32-34\python ./BuildPrjExeSetup.py py2exe
-C:\Utils\Python32-34\python ./BuildEditorApiExeSetup.py py2exe
-C:\Utils\Python32-34\python ./BuildReleaseManifesterUpdaterExeSetup.py py2exe
-C:\Utils\Python32-34\python ./BuildOpenInVisualStudio.py py2exe
+C:\Utils\Python\Python32-34\python ./BuildPrjExeSetup.py py2exe
+C:\Utils\Python\Python32-34\python ./BuildEditorApiExeSetup.py py2exe
+C:\Utils\Python\Python32-34\python ./BuildReleaseManifesterUpdaterExeSetup.py py2exe
+C:\Utils\Python\Python32-34\python ./BuildOpenInVisualStudio.py py2exe
