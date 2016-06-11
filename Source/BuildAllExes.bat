8daaf743827b9706b8c01f7da8ab0b39a4b8135b
artem pecheny <pecheny@gmail.com>
2016-06-11 19:40:00 +0300

    Replace absolute path to python with \%PYTHONHOME\%.

--- Source/BuildAllExes.bat
+++ Source/BuildAllExes.bat
@@ -1,6 +1,9 @@
 @echo off
 REM Make sure to use 32 bit python for this so it runs on all machines
-C:\Utils\Python\Python32-34\python ./BuildPrjExeSetup.py py2exe
-C:\Utils\Python\Python32-34\python ./BuildEditorApiExeSetup.py py2exe
-C:\Utils\Python\Python32-34\python ./BuildReleaseManifesterUpdaterExeSetup.py py2exe
-C:\Utils\Python\Python32-34\python ./BuildOpenInVisualStudio.py py2exe
+%PYTHONHOME%\python ./BuildPrjExeSetup.py py2exe
+%PYTHONHOME%\python ./BuildEditorApiExeSetup.py py2exe
+%PYTHONHOME%\python ./BuildReleaseManifesterUpdaterExeSetup.py py2exe
+%PYTHONHOME%\python ./BuildOpenInVisualStudio.py py2exe
+
+
+
